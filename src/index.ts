54ab1b7576cd26d131c80f84b3bb0330435c1431
Sergio Reinoso <slrs993@gmail.com>
2023-08-07 22:49:51 -0500

    Funciones genericas

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import * as HeroClasses from "./classes/Hero";
-import powers2 from "./data/powers";
+import { printObject, genericFunction, genericFunctionArrow } from "./generics/generics";
 
-const ironman = new HeroClasses.Hero('Ironman', 1, 55);
-console.log(ironman);
-console.log(powers2);
-
-
-
-
-
-
+// printObject(123);
+// printObject(new Date());
+// printObject({a:1, b:2, c:3});
+// printObject([1,2,3,4,5,6,7]);
+// printObject('Hola mundo');
+const name:string = 'Sergio'
+console.log(genericFunctionArrow(3.1416).toFixed(2))
+console.log(genericFunctionArrow(name).toUpperCase())
+console.log(genericFunctionArrow(new Date()).getDate())
 
 
