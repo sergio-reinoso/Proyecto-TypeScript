1f2b7a8cf62a00ecd1ba5da0e471bba643d0f52a
Sergio Reinoso <slrs993@gmail.com>
2023-08-08 23:06:48 -0500

    Mapear respuesta http

---
+++ src/backs/genericos.ts
@@ -0,0 +1,23 @@
+import { printObject, genericFunction, genericFunctionArrow } from "../generics/generics";
+import { Villain, Hero } from "../interfaces";
+
+
+
+// printObject(123);
+// printObject(new Date());
+// printObject({a:1, b:2, c:3});
+// printObject([1,2,3,4,5,6,7]);
+// printObject('Hola mundo');
+// const name:string = 'Sergio';
+
+// console.log(genericFunctionArrow(3.1416).toFixed(2))
+// console.log(genericFunctionArrow(name).toUpperCase())
+// console.log(genericFunctionArrow(new Date()).getDate())
+
+const deadpool = {
+    name: 'Deadpool',
+    realName: 'Wade Winston Wilson',
+    dangerLevel: 130
+}
+
+console.log(genericFunctionArrow<Villain>(deadpool).dangerLevel);
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
-import { printObject, genericFunction, genericFunctionArrow } from "./generics/generics";
-import { Hero } from "./interfaces/hero";
-import { Villain } from "./interfaces/villain";
+import { getPokemon } from "./generics/get-pokemon";
 
-// printObject(123);
-// printObject(new Date());
-// printObject({a:1, b:2, c:3});
-// printObject([1,2,3,4,5,6,7]);
-// printObject('Hola mundo');
-// const name:string = 'Sergio';
-
-// console.log(genericFunctionArrow(3.1416).toFixed(2))
-// console.log(genericFunctionArrow(name).toUpperCase())
-// console.log(genericFunctionArrow(new Date()).getDate())
-
-const deadpool = {
-    name: 'Deadpool',
-    realName: 'Wade Winston Wilson',
-    dangerLevel: 130
-}
-
-console.log(genericFunctionArrow<Villain>(deadpool).dangerLevel);
+getPokemon(4)
+    .then(resp => console.log(resp.name))
+    .catch(error => console.log(error))
+    .finally( () => console.log('Fin de get pokemon') )
